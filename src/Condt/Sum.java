fbdf91185504a36e8fd6ef41ac4f33bbe7c256e7
Acer <Acer@DESKTOP-O070Q13>
2024-03-27 18:07:36 +0530

    new file commit

---
+++ src/Condt/Sum.java
@@ -0,0 +1,17 @@
+package Condt;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1,a=5;
+		do
+		{
+			System.out.println(a+"*"+i+"="+a*i);
+			i++;
+		}
+		while(i<=10);
+	}
+
+}
